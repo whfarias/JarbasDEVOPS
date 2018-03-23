dc1db52ef8e7bf83941b7f252b72352f6c24d525
Walter Farias <whfarias@br.ibm.com>
2018-03-23 14:42:53 -0300

    Alteração da senha

--- LiveUpdateConfig/importConfig.sh
+++ LiveUpdateConfig/importConfig.sh
@@ -3,6 +3,6 @@ counter=0
 while [ $segments_number -gt $counter ]
 do
     segment=$(cat Jarbassegments.txt | python -c 'import json,sys;obj=json.load(sys.stdin);data_str=json.dumps(obj["items"]['$counter']);print data_str;')
-    echo $segment | curl -X POST -d @- --user admin:sAhni_8uxr --header "Content-Type:application/json" https://mobiledemodevopsed-server.eu-gb.mybluemix.net/mfpadmin/management-apis/2.0/runtimes/mfp/admin-plugins/liveUpdateAdapter/com.ibm.codebakery.jarbas/segment
+    echo $segment | curl -X POST -d @- --user admin:pasTeur?mi --header "Content-Type:application/json" https://mobiledemodevopsed-server.eu-gb.mybluemix.net/mfpadmin/management-apis/2.0/runtimes/mfp/admin-plugins/liveUpdateAdapter/com.ibm.codebakery.jarbas/segment
     ((counter++))
 done
